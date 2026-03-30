4c90915a8d0c7ec0debaaeeb8a5018877cdbbc7d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2026-03-30 09:26:10 +0200

    Change default version to 3.0.0

--- MARC2BIBFRAME.pm
+++ MARC2BIBFRAME.pm
@@ -19,7 +19,7 @@ sub new {
 	my $self = bless {}, $class;
 
 	# Version of transformation.
-	$self->{'version'} = '2.5.0';
+	$self->{'version'} = '3.0.0';
 
 	# XSLT transformation file.
 	$self->{'xslt_transformation_file'} = undef;
